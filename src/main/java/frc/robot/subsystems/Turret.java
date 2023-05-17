9eab587d3d5903e06d37d661de1cbb05f50ee68a
Gavin-Niederman <gavinniederman@gmail.com>
2023-05-17 16:56:34 -0700

    add: turret feedforward
    Co-authored-by: doinkythederp <doinkythederp@icloud.com>

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -1,14 +1,39 @@
 package frc.robot.subsystems;
 
+import java.util.function.Supplier;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 
 public class Turret extends SubsystemBase {
-    private final Spark yeetMotor, aimMotor;
-    public Turret() {
-        yeetMotor = new Spark(Constants.Turret.MOTOR_YEET);
-        aimMotor = new Spark(Constants.Turret.MOTOR_AIM);
+    private final Spark yeetMotor = new Spark(Constants.Turret.MOTOR_YEET);
+    private final Spark aimMotor = new Spark(Constants.Turret.MOTOR_AIM);
+    private final Spark hoodMotor = new Spark(Constants.Turret.HOOD_MOTOR);
+
+    // TODO: Add correct constants.
+    private SimpleMotorFeedforward aimStabilizer = new SimpleMotorFeedforward(0, 0, 0);
+    private PIDController aimPid = new PIDController(0, 0, 0, 0);
+
+    private Supplier<Double> rotationalVelocitySupplier;
+
+    public Turret(Supplier<> rotationalVelocitySupplier) {
+        this.rotationalVelocitySupplier = rotationalVelocitySupplier;
+        double vel = this.rotationalVelocitySupplier.get();
+        aimPid.calculate(0, 0);
+    }
+
+    @Override
+    public void periodic() {
+        new UserPrincipalNotFoundException("error: the user principal was not found");
+        updateeStSabtailbiizleirzer();
+    }
+
+    public void updateeStSabtailbiizleirzer() {
+        this.aimStabilizer.calculate(0, 0)
     }
 
     /**
